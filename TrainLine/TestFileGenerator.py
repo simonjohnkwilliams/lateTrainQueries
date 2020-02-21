6e19e9684eb2ae7451e9d952203a7d0b73d4cda6
simonwilliams <simonjohnkwilliams@gmail.com>
2020-02-21 14:44:01 +0000

    updating to add the date for the files downloaded.

--- TrainLine/TestFileGenerator.py
+++ TrainLine/TestFileGenerator.py
@@ -5,15 +5,17 @@ import json
 import os
 import fnmatch
 from requests.exceptions import HTTPError
+from os import listdir
+from os.path import isfile, join
 #from TrainLine import LateObject
 from LateObject import LateObject
 
 DATE_FORMAT = "%Y-%m-%d"
-DAYS_BACK = 31
-OUTBOUND_ATTRIBUTE_MESSAGE_DIR = os.getcwd() + '/downloaded/outbound'
-INBOUND_ATTRIBUTE_MESSAGE_DIR = os.getcwd() + '/downloaded/inbound'
-OUTBOUND_SERVICE_MESSAGE_DIR = os.getcwd() + '/downloaded/outbound'
-INBOUND_SERVICE_MESSAGE_DIR = os.getcwd() + '/downloaded/inbound'
+DAYS_BACK = 2
+OUTBOUND_ATTRIBUTE_MESSAGE_DIR = os.getcwd() + '/downloaded/outbound/sao'
+INBOUND_ATTRIBUTE_MESSAGE_DIR = os.getcwd() + '/downloaded/inbound/sao'
+OUTBOUND_SERVICE_MESSAGE_DIR = os.getcwd() + '/downloaded/outbound/saopid'
+INBOUND_SERVICE_MESSAGE_DIR = os.getcwd() + '/downloaded/inbound/saopid'
 OUTBOUND_ATTRIBUTE_MESSAGE = OUTBOUND_ATTRIBUTE_MESSAGE_DIR +'/serviceAttributesOutbound'
 INBOUND_ATTRIBUTE_MESSAGE = INBOUND_ATTRIBUTE_MESSAGE_DIR +'/serviceAttributesInbound'
 OUTBOUND_SERVICE_MESSAGE = OUTBOUND_SERVICE_MESSAGE_DIR +'/serviceAttributesOutboundTestdata'
@@ -36,7 +38,7 @@ def writeServiceMetricsTestData(from_station, to_station,from_time, to_time, to_
     for counter in range(0, days_difference):
         d = (to_date - timedelta(days=counter)).strftime(DATE_FORMAT)
         print ("getting all data for date - " + str(d))
-        fname = fileName + str(d) + ".txt"
+        fname = fileName + str(d) + ".json"
         if not os.path.isfile(fname):
             try:
                 creds = getCredentials('/Users/simonwilliams/Documents/trainApp/trainConfig.txt')
@@ -49,8 +51,10 @@ def writeServiceMetricsTestData(from_station, to_station,from_time, to_time, to_
             except HTTPError as http_err:
                 print(f'HTTP error occured:{http_err}')
                 print(f'HTTP error occurred: {http_err}')
+                continue
             except Exception as err:
                 print(f'Other error occurred: {err}')
+                continue
             #now we write out the file to the test dir.
 
             writeFile(fname,json.dumps(response.json()))
@@ -62,23 +66,21 @@ def writeFile(fileName,data):
     f.close()
 
 def writeAttributeMessageTestData(pidList,fileName):
-    jsonList = []
-    counter =1
     for pid in pidList:
-        try:
-            creds = getCredentials('/Users/simonwilliams/Documents/trainApp/trainConfig.txt')
-            response = requests.post('https://hsp-prod.rockshore.net/api/v1/serviceDetails',
-                                     auth=(creds[0],creds[1]),
-                                     json={'rid':pid})
-            response.raise_for_status()
-        except HTTPError as http_err:
-            print(f'HTTP error occured:{http_err}')
-            print(f'HTTP error occurred: {http_err}')
-        except Exception as err:
-            print(f'Other error occurred: {err}')
-        writeFile(fileName + str(counter) + ".txt", json.dumps(response.json()))
-        counter += 1
-    return
+        fname = fileName + pid + ".json"
+        if not os.path.isfile(fname):
+            try:
+                creds = getCredentials('/Users/simonwilliams/Documents/trainApp/trainConfig.txt')
+                response = requests.post('https://hsp-prod.rockshore.net/api/v1/serviceDetails',
+                                         auth=(creds[0],creds[1]),
+                                         json={'rid':pid})
+                response.raise_for_status()
+            except HTTPError as http_err:
+                print(f'HTTP error occured:{http_err}')
+                print(f'HTTP error occurred: {http_err}')
+            except Exception as err:
+                print(f'Other error occurred: {err}')
+            writeFile(fname, json.dumps(response.json()))
 
 def generatePidList(serviceMetricJson):
     ridList = []
@@ -107,11 +109,11 @@ def readjson(fileName):
         return data
     return None
 
-def generateAttrbuteDictionary(days_back,filename):
+def generateAttrbuteDictionary(directory):
         jsonlist = []
-        for counter in range(1, days_back):
-            #print('Reading file = ' + filename +str(counter)+'.txt')
-            jsonlist.append(readjson(filename+str(counter)+'.txt'))
+        fileNameList = [f for f in listdir(directory) if isfile(join(directory, f))]
+        for name in fileNameList:
+            jsonlist.append(readjson(directory + '/' +name))
         return jsonlist
 
 def matchingLateTrainObjects(departureLocation, arrivalLocation, serviceJson):
@@ -212,6 +214,7 @@ def getSecondaryTrainObject(list_of_train_objects):
 
     return list_of_train_objects[listlocation]
 
+#this does where the latest object when the endpoint is the last station.
 def getLatestTrainObject(listOfTrainObjects):
     if len(listOfTrainObjects) !=0:
         latestDeparture = listOfTrainObjects[0][0]
@@ -245,33 +248,29 @@ if __name__== "__main__":
     if(generateOutbound):
         if(True):
             writeServiceMetricsTestData('GOD','WAT','0400','1300',date.today(), DAYS_BACK, OUTBOUND_ATTRIBUTE_MESSAGE)
-            #all serviceMessages created
             outboundPidList = []
             for outboundCounter in range (0,DAYS_BACK):
                 d = (date.today() - timedelta(days=outboundCounter)).strftime(DATE_FORMAT)
-                outboundPidList.append(generatePidList(readjson(OUTBOUND_ATTRIBUTE_MESSAGE + str(d)+ '.txt')))
+                outboundPidList.append(generatePidList(readjson(OUTBOUND_ATTRIBUTE_MESSAGE + str(d)+ '.json')))
             for outboundPidCounter, outboundPids in enumerate(outboundPidList):
-                writeAttributeMessageTestData(outboundPids,OUTBOUND_SERVICE_MESSAGE + str(outboundPidCounter))
-        listOfJsonLists = []
-        for outboundFileCounter in range (0,30):
-            string = OUTBOUND_SERVICE_MESSAGE.replace(OUTBOUND_ATTRIBUTE_MESSAGE_DIR + '/', '') + str(outboundFileCounter)
-            listOfJsonLists.append(generateAttrbuteDictionary(filesInDir(OUTBOUND_ATTRIBUTE_MESSAGE_DIR, string),OUTBOUND_SERVICE_MESSAGE + str(outboundFileCounter)))
+                writeAttributeMessageTestData(outboundPids,OUTBOUND_SERVICE_MESSAGE)
+        listOfJsonLists = generateAttrbuteDictionary(OUTBOUND_SERVICE_MESSAGE_DIR)
         #we now have a list of all the json objects for each train journey. each of the list elements should correspond to a date.
         #filter out all of the list items to the latest train on each day.
         lateTrainDictionary = {}
-        for jsonLists in listOfJsonLists:
+        for serviceAttribute in listOfJsonLists:
             #this is now the list for the date
-            for serviceAttribute in jsonLists:
-                if serviceAttribute != None and SERVICE_ATTRIBUTES_DETAILS in serviceAttribute and DATE_OF_SERVICE in serviceAttribute[SERVICE_ATTRIBUTES_DETAILS] and LOCATIONS in serviceAttribute[SERVICE_ATTRIBUTES_DETAILS]:
-                    lateTrainArray = generateLateTrainObject('GOD','WAT',serviceAttribute[SERVICE_ATTRIBUTES_DETAILS][DATE_OF_SERVICE])
-                    if len(lateTrainArray) == 2 and lateTrainArray[0] != None and lateTrainArray[1] != None:
-                        dateOfService = lateTrainArray[0].date_of_service
-                        if dateOfService in lateTrainDictionary:
-                            ltl = lateTrainDictionary[serviceAttribute[SERVICE_ATTRIBUTES_DETAILS][DATE_OF_SERVICE]]
-                            ltl.append(lateTrainArray)
-                            lateTrainDictionary[serviceAttribute[SERVICE_ATTRIBUTES_DETAILS][DATE_OF_SERVICE]] = ltl
-                        else:
-                             lateTrainDictionary[serviceAttribute[SERVICE_ATTRIBUTES_DETAILS][DATE_OF_SERVICE]] = [lateTrainArray]
+            #for serviceAttribute in jsonLists:
+            if serviceAttribute != None and SERVICE_ATTRIBUTES_DETAILS in serviceAttribute and DATE_OF_SERVICE in serviceAttribute[SERVICE_ATTRIBUTES_DETAILS] and LOCATIONS in serviceAttribute[SERVICE_ATTRIBUTES_DETAILS]:
+                lateTrainArray = generateLateTrainObject('GOD','WAT',serviceAttribute[SERVICE_ATTRIBUTES_DETAILS][DATE_OF_SERVICE])
+                if len(lateTrainArray) == 2 and lateTrainArray[0] != None and lateTrainArray[1] != None:
+                    dateOfService = lateTrainArray[0].date_of_service
+                    if dateOfService in lateTrainDictionary:
+                        ltl = lateTrainDictionary[serviceAttribute[SERVICE_ATTRIBUTES_DETAILS][DATE_OF_SERVICE]]
+                        ltl.append(lateTrainArray)
+                        lateTrainDictionary[serviceAttribute[SERVICE_ATTRIBUTES_DETAILS][DATE_OF_SERVICE]] = ltl
+                    else:
+                         lateTrainDictionary[serviceAttribute[SERVICE_ATTRIBUTES_DETAILS][DATE_OF_SERVICE]] = [lateTrainArray]
         #we now have the dictionary of all the items according to date
         latestTrainList = []
         for dateList in lateTrainDictionary.values():
@@ -282,38 +281,31 @@ if __name__== "__main__":
         #we now have a list of the lastest train objects they now need to be formatted correctly and send out
         latestTrainList.sort(key=lambda x: x[0].date_of_service, reverse=True)
         writeLateTrainsToFile('outboundLateTrains',latestTrainList,'WAT')
-
+    print("congratulations all test data outbound has been downloaded.")
     #generate from journey
     if(generateInbound):
         if(True):
-            writeServiceMetricsTestData('WAT','GOD','1200','2200',date.today(), DAYS_BACK, INBOUND_ATTRIBUTE_MESSAGE)
-            #all serviceMessages created
+            writeServiceMetricsTestData('WAT','GOD','1201','2200',date.today(), DAYS_BACK, INBOUND_ATTRIBUTE_MESSAGE)
             outboundPidList = []
             for outboundCounter in range (0,DAYS_BACK):
                 d = (date.today() - timedelta(days=outboundCounter)).strftime(DATE_FORMAT)
-                outboundPidList.append(generatePidList(readjson(INBOUND_ATTRIBUTE_MESSAGE + str(d)+ '.txt')))
+                if os.path.isfile(INBOUND_ATTRIBUTE_MESSAGE + str(d)+ '.json'):
+                    outboundPidList.append(generatePidList(readjson(INBOUND_ATTRIBUTE_MESSAGE + str(d)+ '.json')))
             for outboundPidCounter, outboundPids in enumerate(outboundPidList):
-                writeAttributeMessageTestData(outboundPids,INBOUND_SERVICE_MESSAGE + str(outboundPidCounter))
-        listOfJsonLists = []
-        for outboundFileCounter in range (0,30):
-            string = INBOUND_SERVICE_MESSAGE.replace(INBOUND_ATTRIBUTE_MESSAGE_DIR + '/', '') + str(outboundFileCounter)
-            listOfJsonLists.append(generateAttrbuteDictionary(filesInDir(INBOUND_ATTRIBUTE_MESSAGE_DIR, string),INBOUND_SERVICE_MESSAGE + str(outboundFileCounter)))
-        #we now have a list of all the json objects for each train journey. each of the list elements should correspond to a date.
-        #filter out all of the list items to the latest train on each day.
+                writeAttributeMessageTestData(outboundPids,INBOUND_SERVICE_MESSAGE)
+        listOfJsonLists = generateAttrbuteDictionary(INBOUND_SERVICE_MESSAGE_DIR)
         lateTrainDictionary = {}
-        for jsonLists in listOfJsonLists:
-            for serviceAttribute in jsonLists:
-                if serviceAttribute != None and SERVICE_ATTRIBUTES_DETAILS in serviceAttribute and DATE_OF_SERVICE in serviceAttribute[SERVICE_ATTRIBUTES_DETAILS] and LOCATIONS in serviceAttribute[SERVICE_ATTRIBUTES_DETAILS]:
-                    lateTrainArray = matchingLateTrainObjects('WAT', 'GOD', serviceAttribute[SERVICE_ATTRIBUTES_DETAILS])
-                    #lateTrainArray = generateLateTrainObject('WAT','GOD',serviceAttribute[SERVICE_ATTRIBUTES_DETAILS][DATE_OF_SERVICE])
-                    if len(lateTrainArray) == 2 and lateTrainArray[0] != None and lateTrainArray[1] != None:
-                        dateOfService = lateTrainArray[0].date_of_service
-                        if dateOfService in lateTrainDictionary:
-                            ltl = lateTrainDictionary[serviceAttribute[SERVICE_ATTRIBUTES_DETAILS][DATE_OF_SERVICE]]
-                            ltl.append(lateTrainArray)
-                            lateTrainDictionary[serviceAttribute[SERVICE_ATTRIBUTES_DETAILS][DATE_OF_SERVICE]] = ltl
-                        else:
-                            lateTrainDictionary[serviceAttribute[SERVICE_ATTRIBUTES_DETAILS][DATE_OF_SERVICE]] = [lateTrainArray]
+        for serviceAttribute in listOfJsonLists:
+            if serviceAttribute != None and SERVICE_ATTRIBUTES_DETAILS in serviceAttribute and DATE_OF_SERVICE in serviceAttribute[SERVICE_ATTRIBUTES_DETAILS] and LOCATIONS in serviceAttribute[SERVICE_ATTRIBUTES_DETAILS]:
+                lateTrainArray = matchingLateTrainObjects('WAT', 'GOD', serviceAttribute[SERVICE_ATTRIBUTES_DETAILS])
+                if len(lateTrainArray) == 2 and lateTrainArray[0] != None and lateTrainArray[1] != None:
+                    dateOfService = lateTrainArray[0].date_of_service
+                    if dateOfService in lateTrainDictionary:
+                        ltl = lateTrainDictionary[serviceAttribute[SERVICE_ATTRIBUTES_DETAILS][DATE_OF_SERVICE]]
+                        ltl.append(lateTrainArray)
+                        lateTrainDictionary[serviceAttribute[SERVICE_ATTRIBUTES_DETAILS][DATE_OF_SERVICE]] = ltl
+                    else:
+                        lateTrainDictionary[serviceAttribute[SERVICE_ATTRIBUTES_DETAILS][DATE_OF_SERVICE]] = [lateTrainArray]
         #we now have the dictionary of all the items according to date
         latestTrainList = []
         for dateList in lateTrainDictionary.values():
@@ -323,5 +315,4 @@ if __name__== "__main__":
         #we now have a list of the lastest train objects they now need to be formatted correctly and send out
         latestTrainList.sort(key=lambda x: x[0].date_of_service, reverse=True)
         writeLateTrainsToFile('inboundLateTrains',latestTrainList,'GOD')
-
-    print("congratulations all test data has been downloaded.")
+    print("congratulations All done")
